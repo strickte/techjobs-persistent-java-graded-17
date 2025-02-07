3ac7f9541192cf8974ccd08dc021d7150fe6707d
strickte <166240135+strickte@users.noreply.github.com>
2025-02-06 20:25:58 -0600

    Passing 5 of 10 Task 4 tests, at very beginning of 'Updating HomeController, Again'

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -1,11 +1,9 @@
 package org.launchcode.techjobs.persistent.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -13,17 +11,17 @@ public class Job extends AbstractEntity{
 
     @ManyToOne
     private Employer employer;
-    private String skills;
 
+    @ManyToMany
+    private final List<Skill> skills = new ArrayList<>();
 
     public Job() {
     }
 
     // Initialize the id and value fields.
-    public Job(Employer employer, String someSkills) {
+    public Job(Employer employer, Skill someSkills) {
         super();
         this.employer = employer;
-        this.skills = someSkills;
     }
 
     // Getters and setters.
@@ -37,12 +35,12 @@ public class Job extends AbstractEntity{
         this.employer = employer;
     }
 
-    public String getSkills() {
+    public List<Skill> getSkills() {
         return skills;
     }
 
-    public void setSkills(String skills) {
-        this.skills = skills;
+    public void setSkills(List<Skill> skill) {
+        this.skills.add((Skill) skill);
     }
 
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,15 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
     @Size(max = 500)
     private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private final List<Job> jobs = new ArrayList<>();
 
     public Skill() {}
 
@@ -19,5 +26,13 @@ public class Skill extends AbstractEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void addJob(Job job) {
+        this.jobs.add(job);
     }
 }
